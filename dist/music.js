65d3501ee260f3df635514e6bb5f821901fb4944
Dong <2324298849@qq.com>
2020-06-29 13:31:46 +0800

    Site updated: 2020-06-29 13:31:45

---
+++ dist/music.js
@@ -0,0 +1,32 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  lrcType: 0,
+  audio: [
+    {
+      name: '无尽光芒',
+      artist: '许巍',
+      url: 'https://www.liaofuzhan.com/music/无尽光芒.mp3',
+      cover: 'https://s2.ax1x.com/2019/10/03/uBPBHf.jpg',
+    },
+    {
+      name: '夕阳中的城市',
+      artist: '许巍',
+      url: 'https://www.liaofuzhan.com/music/夕阳中的城市.mp3',
+      cover: 'https://s2.ax1x.com/2019/10/03/uBPBHf.jpg',
+    },
+    {
+      name: '心中的歌谣',
+      artist: '许巍',
+      url: 'https://www.liaofuzhan.com/music/心中的歌谣.mp3',
+      cover: 'https://s2.ax1x.com/2019/10/03/uBPBHf.jpg',
+    },
+    {
+      name: '远航',
+      artist: '许巍',
+      url: 'https://www.liaofuzhan.com/music/远航.mp3',
+      cover: 'https://s2.ax1x.com/2019/10/03/uBPBHf.jpg',
+    }
+  ]
+});
